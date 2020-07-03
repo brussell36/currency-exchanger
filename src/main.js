efb5e0666cfe2548f7b45d87428df41bd4c09177
Ben Russell <benjaminrussell36@gmail.com>
2020-07-03 12:06:15 -0700

    Take out dropdown and replace with text input.

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import { ExchangeService } from './../src/currency-exchange.js';
 
 $(document).ready(function() {
   $('#exchangeCurrency').click(function() {
-    const currency = $('#country').val();
+    const currency = $('.country').val();
     const amount = $('#amount').val();
-    $('#country').val("");
+    $('.country').val("");
     $('#amount').val("");
 
     (async () => {
