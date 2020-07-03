c8a36e1fbefa807c526561f4ea9f0374f40932c7
Ben Russell <benjaminrussell36@gmail.com>
2020-07-03 09:12:05 -0700

    Add UI logic to main.js for showing output to user.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from "jquery";
+import $ from "jquery";
+import { ExchangeService } from './../src/currency-exchange.js';
+
+$(document).ready(function() {
+
+  $('#exchangeCurrency').click(function() {
+    const currency = $('#country').val();
+    $('#currency').val("");
+
+    (async () => {
+      let exchangeService = new ExchangeService();
+      const response = await exchangeService.getRateByCurrency(currency);
+      getRate(response);
+    })();
+
+    function getRate(response) {
+      if (response) {
+        $('.showCurrency').text(`The current value of ${currency} is ${response.conversion_rates.currency}`);
+      } else {
+        $('.showCurrency').text(`There was an error handling your request.`);
+      }
+    }
+  });
+});
